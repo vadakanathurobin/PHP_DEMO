300f36b98284bdbde0bce7b51ac201a9641def42
b <l>
2022-05-18 09:16:11 +0530

    php code

--- docker-files/docker-script.sh
+++ docker-files/docker-script.sh
@@ -2,4 +2,3 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
