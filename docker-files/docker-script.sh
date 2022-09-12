fba44e29a1aac875c2f06654332ac0c5bed46600
Preethi <28048073+preethid@users.noreply.github.com>
2022-09-12 07:04:55 +0530

    Update docker-script.sh

--- docker-files/docker-script.sh
+++ docker-files/docker-script.sh
@@ -1,4 +1,3 @@
+#!/usr/bin/env bash
 sudo yum install docker -y
 sudo systemctl start docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
